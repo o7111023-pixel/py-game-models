5ac08725bc9a099d5cfef3b36d7aa814e41d72ff
Oleh M <oleh.murachov28@gmail.com>
2026-04-21 07:29:33 +0200

    okl

--- main.py
+++ main.py
@@ -5,43 +5,43 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main():
-    with open('players.json', 'r', encoding='utf-8') as file:
+    with open("players.json", "r", encoding="utf-8") as file:
         data = json.load(file)
 
     for nickname, item in data.items():
-        race_data = item.get('race')
+        race_data = item.get("race")
         race, _ = Race.objects.get_or_create(
-            name=race_data['name'],
+            name=race_data["name"],
             defaults={
-                'description': race_data.get('description', '')
+                "description": race_data.get("description", "")
             }
         )
 
-        for skill_data in race_data.get('skills', []):
+        for skill_data in race_data.get("skills", []):
             Skill.objects.get_or_create(
-                name=skill_data['name'],
+                name=skill_data["name"],
                 defaults={
-                    'bonus': skill_data['bonus'],
-                    'race': race
+                    "bonus": skill_data["bonus"],
+                    "race": race
                 }
             )
 
-        guild_data = item.get('guild')
+        guild_data = item.get("guild")
         guild = None
         if guild_data:
             guild, _ = Guild.objects.get_or_create(
-                name=guild_data['name'],
+                name=guild_data["name"],
                 defaults={
-                    'description': guild_data.get('description')
+                    "description": guild_data.get("description")
                 }
             )
 
         Player.objects.get_or_create(
             nickname=nickname,
             defaults={
-                'email': item['email'],
-                'bio': item['bio'],
-                'race': race,
-                'guild': guild
+                "email": item["email"],
+                "bio": item["bio"],
+                "race": race,
+                "guild": guild
             }
         )
